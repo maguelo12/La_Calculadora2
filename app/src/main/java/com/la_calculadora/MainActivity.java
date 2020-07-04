a3b62f3e8a8e31ba12d6833506204ff35717d26c
maguelo12 <67301362+maguelo12@users.noreply.github.com>
2020-07-04 14:59:02 -0400

    Cal_cu_la_do_ra

--- app/src/main/java/com/la_calculadora/MainActivity.java
+++ app/src/main/java/com/la_calculadora/MainActivity.java
@@ -11,7 +11,11 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     EditText num1;
+    EditText num2;
     Button suma;
+    Button rest;
+    Button divid;
+    Button multiplica;
     TextView resultado;
 
     @Override
@@ -20,18 +24,65 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         num1= (EditText)findViewById(R.id.numero1);
+        num2= (EditText)findViewById(R.id.numero2);
 
         resultado=(TextView)findViewById(R.id.resultado);
 
         suma=(Button)findViewById(R.id.btnSuma);
+        rest=(Button)findViewById(R.id.restar);
+        divid=(Button)(findViewById(R.id.dividir)) ;
+        multiplica=(Button)(findViewById(R.id.multiplicar));
 
         suma.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
                 double numero1= Double.parseDouble(num1.getText().toString());
+                double numero2= Double.parseDouble(num2.getText().toString());
 
-                double sumatoria= numero1 + 10;
+                double sumatoria= numero1 + numero2;
+
+                resultado.setText(Double.toString(sumatoria));
+
+            }
+        });
+
+        rest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                double numero1= Double.parseDouble(num1.getText().toString());
+                double numero2= Double.parseDouble(num2.getText().toString());
+
+                double sumatoria= numero1 - numero2;
+
+                resultado.setText(Double.toString(sumatoria));
+
+            }
+        });
+
+        divid.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                double numero1= Double.parseDouble(num1.getText().toString());
+                double numero2= Double.parseDouble(num2.getText().toString());
+
+                double sumatoria= numero1 / numero2;
+
+                resultado.setText(Double.toString(sumatoria));
+
+            }
+        });
+
+        multiplica.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                double numero1= Double.parseDouble(num1.getText().toString());
+                double numero2= Double.parseDouble(num2.getText().toString());
+
+                double sumatoria= numero1 * numero2;
 
                 resultado.setText(Double.toString(sumatoria));
 
